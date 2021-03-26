99a220e4aa6c19f6f248f1ef0670e2a26d1dca0e
Ralph Tamayo <ralphtamayo@creatinginfo.com>
2021-03-26 12:04:03 +0800

    Add bank account GET endpoints

--- api/src/controllers/bank-account.controller.ts
+++ api/src/controllers/bank-account.controller.ts
@@ -3,33 +3,43 @@ import { validateId } from '../middleware/validate-id';
 import { Customer } from '../models/customer.model';
 import { FormUtils } from '../utils/form-utils';
 import { CustomResponse } from '../utils/custom-response';
+import { BankAccount } from '../models/bank-account.model';
 
 export const router = Router();
 
 /**
- * TODO: Bank Accout GET endpoints.
+ * GET /api/bank-account
  */
+router.get('/', async (req: Request, res: Response) => {
+	const bankAccounts = await BankAccount.find().select('balance customer');
 
-// /**
-//  * GET /api/bank-account
-//  */
-// router.get('/', async (req: Request, res: Response) => {
-// 	const bankAccounts = await Customer.find().select('firstName lastName');
+	res.send(bankAccounts);
+});
 
-// 	res.send(bankAccounts);
-// });
+/**
+ * GET /api/bank-account/:id
+ */
+router.get('/:id', validateId, async (req: Request, res: Response) => {
+	const bankAccount = await BankAccount.findById(req.params.id).select('balance customer').populate('customer', 'firstName lastName');
 
-// /**
-//  * GET /api/bank-account/:id
-//  */
-// router.get('/:id', validateId, async (req: Request, res: Response) => {
-// 	const bankAccount = await Customer.findById(req.params.id).select('firstName lastName');
+	if (bankAccount == null) {
+		return res.status(404).send(CustomResponse.createEntityNotFoundError());
+	}
 
-// 	if (bankAccount == null) {
-// 		return res.status(404).send(CustomResponse.createEntityNotFoundError());
-// 	}
+	return res.status(200).send(bankAccount);
+});
 
-// 	return res.status(200).send(bankAccount);
-// });
+/**
+ * GET /api/bank-account/:id/balance
+ */
+router.get('/:id/balance', validateId, async (req: Request, res: Response) => {
+	const bankAccount = await BankAccount.findById(req.params.id).select('balance');
+
+	if (bankAccount == null) {
+		return res.status(404).send(CustomResponse.createEntityNotFoundError());
+	}
+
+	return res.status(200).send(bankAccount);
+});
 
 export { router as bankAccountRouter }
